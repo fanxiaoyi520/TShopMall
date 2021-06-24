6ead3dbd5bf52acc983c5844589c938587f2bb9e
余海 <>
2021-06-24 17:02:29 +0800

    对接 微信登录 苹果登录

---
+++ TShopMall/TShopMall/Expand/Category/UIKit/Popover+TSDefaultConfig.h
@@ -0,0 +1,16 @@
+//
+//  Popover+TSDefaultConfig.h
+//  TShopMall
+//
+//  Created by  on 2021/6/24.
+//
+
+#import "Popover.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Popover (TSDefaultConfig)
++ (ProgressModel *)defaultConfig;
+@end
+
+NS_ASSUME_NONNULL_END
