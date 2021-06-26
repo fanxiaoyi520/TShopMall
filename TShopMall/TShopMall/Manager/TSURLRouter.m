9f4058335149e7db66c638e935d5ab0a1967ec5a
余海 <hai6.yu@tcl.com>
2021-06-26 14:24:05 +0800

    增加 首页 路由 跳转

---
+++ TShopMall/TShopMall/Manager/TSURLRouter.m
@@ -0,0 +1,87 @@
+//
+//  TSURLRouter.m
+//  TShopMall
+//
+//  Created by  on 2021/6/26.
+//
+
+#import "TSURLRouter.h"
+#import "NSString+Plugin.h"
+#import "NSObject+TSProperty.h"
+
+@implementation TSURLRouter
++ (instancetype)sharedInstance {
+    static dispatch_once_t pred;
+    static id instance = nil;
+    dispatch_once(&pred, ^{
+        instance = [[self alloc] init];
+    });
+    return instance;
+}
+
+- (UIViewController *)getCurrentVC{
+    
+    UIWindow * window = [[UIApplication sharedApplication] keyWindow];
+    //app默认windowLevel是UIWindowLevelNormal，如果不是，找到UIWindowLevelNormal的
+    //其他框架可能会改我们的keywindow，比如支付宝支付，qq登录都是在一个新的window上，这时候的keywindow就不是appdelegate中的window。 当然这里也可以直接用APPdelegate里的window。
+    if (window.windowLevel != UIWindowLevelNormal)
+    {
+        NSArray *windows = [[UIApplication sharedApplication] windows];
+        for(UIWindow * tmpWin in windows)
+        {
+            if (tmpWin.windowLevel == UIWindowLevelNormal)
+            {
+                window = tmpWin;
+                break;
+            }
+        }
+    }
+    
+    UIViewController* currentViewController = window.rootViewController;
+    while (YES) {
+        if (currentViewController.presentedViewController) {
+            currentViewController = currentViewController.presentedViewController;
+        } else {
+            if ([currentViewController isKindOfClass:[UINavigationController class]]) {
+                currentViewController = ((UINavigationController *)currentViewController).visibleViewController;
+            } else if ([currentViewController isKindOfClass:[UITabBarController class]]) {
+                currentViewController = ((UITabBarController* )currentViewController).selectedViewController;
+            } else {
+                break;
+            }
+        }
+    }
+    
+    return currentViewController;
+}
+
+- (void)openURI:(NSString *_Nullable)uri {
+    NSLog(@"AppDelegate 中 uri==%@",uri);
+    
+    Class cls = NSClassFromString([self getClassDict][[uri componentsSeparatedByString:@"?"].firstObject]);
+    if (cls == nil) { return; }
+    
+    UIViewController *obj = [[cls alloc] init];
+    if (obj == nil || ![obj isKindOfClass:[UIViewController class]]) {
+        return;
+    }
+
+    for (NSString *propertyKey in obj.ts_validProperties) {
+        NSObject *propertyValue = [uri.ts_urlParsing objectForKey:propertyKey];
+        if (propertyValue) {
+            [obj setValue:propertyValue forKey:propertyKey];
+        }
+    }
+    
+    [self.getCurrentVC.navigationController pushViewController:obj animated:YES];
+}
+
+- (NSDictionary *)getClassDict {
+    return @{
+        @"page://quote/productDetail": @"TSProductDetailController",
+        @"page://quote/category": @"TSCategoryViewController",
+        @"page://quote/searchResult": @"TSSearchResultController",
+
+    };
+}
+@end
