645262d196855795810da238dec5da308910fd9d
余海 <hai6.yu@tcl.com>
2021-06-28 16:10:08 +0800

    排行榜 代码 对接

--- TShopMall/TShopMall/Features/Message/View/TSRankHeaderView.h
+++ TShopMall/TShopMall/Features/Message/View/TSRankHeaderView.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface TSRankHeaderView : TSUniversalHeaderView
+@interface TSRankHeaderView : UITableViewHeaderFooterView
 
 @end
 
---
+++ TShopMall/TShopMall/Features/Message/View/TSRankRecommendCell.m
@@ -0,0 +1,50 @@
+//
+//  TSRankRecommendCell.m
+//  TShopMall
+//
+//  Created by  on 2021/6/28.
+//
+
+#import "TSRankRecommendCell.h"
+#import "TSRecomendGoodsView.h"
+#import "TSRankSectionModel.h"
+
+@interface TSRankRecommendCell ()
+@property(nonatomic, strong) TSRecomendGoodsView *goodsView;
+
+@end
+
+@implementation TSRankRecommendCell
+
+- (void)setupUI{
+    self.contentView.backgroundColor = KWhiteColor;
+    [self.contentView addSubview:self.goodsView];
+    [self.goodsView mas_remakeConstraints:^(MASConstraintMaker *make) {
+       make.edges.equalTo(self.contentView);
+    }];
+}
+
+- (void)setData:(id)data{
+    [super setData:data];
+    
+    TSRankSectionItemModel *item = (TSRankSectionItemModel *)data;
+    @weakify(self);
+    if (!item.datas.count) {
+        [self.goodsView getRecommendListWithType:@"cart_page" success:^(NSArray * _Nullable list) {
+            @strongify(self)
+            item.datas = list;
+            [self tableviewReloadCell];
+        }];
+
+    }else{
+        self.goodsView.items = item.datas;
+    }
+}
+
+- (TSRecomendGoodsView *)goodsView{
+    if (!_goodsView) {
+        _goodsView = [TSRecomendGoodsView new];
+    }
+    return _goodsView;
+}
+@end
