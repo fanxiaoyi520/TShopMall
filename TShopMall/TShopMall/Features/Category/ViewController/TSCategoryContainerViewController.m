72c0277d3f847a0648b6a4b8eb528c43eed4aedd
余海 <hai6.yu@tcl.com>
2021-06-29 20:43:27 +0800

    重写 分类 vc

---
+++ TShopMall/TShopMall/Features/Category/ViewController/TSCategoryContainerViewController.m
@@ -0,0 +1,59 @@
+//
+//  TSCategoryContainerViewController.m
+//  TShopMall
+//
+//  Created by  on 2021/6/29.
+//
+
+#import "TSCategoryContainerViewController.h"
+
+@interface TSCategoryContainerViewController ()
+@property (nonatomic, strong) NSMutableArray <UIView*> *containers;
+@end
+
+@implementation TSCategoryContainerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    [self hiddenNavigationBar];
+    
+}
+
+- (void)setDataSource:(id<TSCategoryContainerDataSource>)dataSource{
+    _dataSource = dataSource;
+    if (_dataSource && [_dataSource respondsToSelector:@selector(numberOfContentsInContainerView:)]) {
+        NSInteger count = [_dataSource numberOfContentsInContainerView:self];
+        for (int i = 0; i < count; i ++) {
+            if (_dataSource && [_dataSource respondsToSelector:@selector(viewForContainerViewController:)]) {
+                UIView *view = [_dataSource viewForContainerViewController:self];
+//                [self.view addSubview:view];
+                [self.containers addObject:view];
+                
+            }
+        }
+    }
+}
+
+- (void)showContentAtPage:(NSInteger)page{
+    if (self.containers.count) {
+        [self.view removeAllSubviews];
+        
+        UIView *view = self.containers[page];
+        [self.view addSubview:view];
+        [view mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.edges.equalTo(self.view);
+        }];
+    }
+   
+}
+
+- (NSMutableArray *)containers{
+    if (!_containers) {
+        _containers = [NSMutableArray array];
+    }
+    return _containers;
+}
+
+@end
