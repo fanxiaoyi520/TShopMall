14f2e9d19fdc0c39572ccccf37d7c1c304285591
余海 <hai6.yu@tcl.com>
2021-06-30 17:52:48 +0800

    优化首页 代码

--- TShopMall/TShopMall/Features/Home/View/TSHomePageReleaseCell.m
+++ TShopMall/TShopMall/Features/Home/View/TSHomePageReleaseCell.m
@@ -11,26 +11,27 @@
 #import "TSImageBaseModel.h"
 #import "TSRecomendGoodsView.h"
 @interface TSHomePageReleaseCell()
+@property(nonatomic, strong) NSMutableArray *iconButtons;
 
-@property(nonatomic, strong) UIButton *iconButton;
-@property(nonatomic, strong) UILabel *nameLabel;
-@property(nonatomic, strong) TSRecomendGoodsView *goodsView;
+//@property(nonatomic, strong) UIButton *iconButton;
+//@property(nonatomic, strong) UILabel *nameLabel;
+//@property(nonatomic, strong) TSRecomendGoodsView *goodsView;
 @property(nonatomic, strong) TSHomePageReleaseViewModel *releaseViewModel;
 
 @end
 
 @implementation TSHomePageReleaseCell
 -(void)setupUI{
-    [self.contentView addSubview:self.iconButton];
-    CGFloat height = kScreenWidth/345 * 447;
-
-    [self.iconButton mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.contentView).offset(16);
-        make.right.equalTo(self.contentView).offset(-16);
-        make.top.equalTo(self.contentView);
-        make.height.equalTo(@(height));
-        make.bottom.equalTo(self.contentView).priorityLow();
-    }];
+//    [self.contentView addSubview:self.iconButton];
+//    CGFloat height = kScreenWidth/345 * 447;
+//
+//    [self.iconButton mas_remakeConstraints:^(MASConstraintMaker *make) {
+//        make.left.equalTo(self.contentView).offset(16);
+//        make.right.equalTo(self.contentView).offset(-16);
+//        make.top.equalTo(self.contentView);
+//        make.height.equalTo(@(height));
+//        make.bottom.equalTo(self.contentView).priorityLow();
+//    }];
     
 //    [self.contentView addSubview:self.goodsView];
 //
@@ -57,53 +58,92 @@
     
     TSHomePageReleaseViewModel *releaseViewModel = (TSHomePageReleaseViewModel *)viewModel;
 
-    if (!releaseViewModel.releaseModel) {
+    if (!releaseViewModel.ReleaseDatas) {
         [releaseViewModel getReleaseData];
     }
     @weakify(self);
-    [self.KVOController observe:releaseViewModel keyPath:@"releaseModel" options:(NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew) block:^(id  _Nullable observer, id  _Nonnull object, NSDictionary<NSString *,id> * _Nonnull change) {
+    [self.KVOController observe:releaseViewModel keyPath:@"ReleaseDatas" options:(NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew) block:^(id  _Nullable observer, id  _Nonnull object, NSDictionary<NSString *,id> * _Nonnull change) {
         @strongify(self)
-        if (releaseViewModel.releaseModel) {
-            CGFloat height = kScreenWidth/releaseViewModel.releaseModel.imageData.width * releaseViewModel.releaseModel.imageData.height;
-            [self.iconButton mas_updateConstraints:^(MASConstraintMaker *make) {
-                make.height.equalTo(@(height));
-            }];
-            [self.iconButton sd_setImageWithURL:[NSURL URLWithString:releaseViewModel.releaseModel.imageData.url] forState:UIControlStateNormal];
+        if (releaseViewModel.ReleaseDatas.count) {
+            [self.iconButtons removeAllObjects];
+            for (TSImageBaseModel *model in releaseViewModel.ReleaseDatas) {
+                UIButton *iconButton = [[UIButton alloc] init];
+                iconButton.backgroundColor = [UIColor clearColor];
+                iconButton.clipsToBounds = YES;
+                iconButton.layer.cornerRadius = 8;
+                [iconButton addTarget:self action:@selector(clickAction) forControlEvents:UIControlEventTouchUpInside];
+                iconButton.adjustsImageWhenHighlighted = NO;
+                [self.contentView addSubview:iconButton];
+                [iconButton sd_setImageWithURL:[NSURL URLWithString:model.imageData.url] forState:UIControlStateNormal];
+                [self.iconButtons addObject:iconButton];
+            }
+            
+            for (int i = 0; i < self.iconButtons.count; i ++) {
+                TSImageBaseModel *model = releaseViewModel.ReleaseDatas[i];
+                UIButton *iconButton = self.iconButtons[i];
+                CGFloat top = 0.0;
+                if (i == 0) {
+                    top = 0;
+                }
+                else{
+                    UIButton *iconButton = self.iconButtons[i-1];
+                    top = iconButton.height;
+                }
+                CGFloat height = (kScreenWidth-32)/model.imageData.width * model.imageData.height;
+                [iconButton mas_makeConstraints:^(MASConstraintMaker *make) {
+                    make.top.equalTo(@(top));
+                    make.centerX.equalTo(self.contentView);
+                    make.width.equalTo(@(kScreenWidth-32));
+                    make.height.equalTo(@(height));
+                    if (self.iconButtons.count-1 == i) {
+                        make.bottom.equalTo(self.contentView);
+                    }
+                    
+                }];
+                [iconButton layoutIfNeeded];
+            }
         }
     }];
 }
 
 - (void)clickAction{
-    TSHomePageReleaseViewModel *releaseViewModel = (TSHomePageReleaseViewModel *)self.viewModel;
-    NSLog(@"uri:%@", releaseViewModel.releaseModel.linkData.objectValue);
+//    TSHomePageReleaseViewModel *releaseViewModel = (TSHomePageReleaseViewModel *)self.viewModel;
+//    NSLog(@"uri:%@", releaseViewModel.releaseModel.linkData.objectValue);
 }
 
 #pragma mark - Getter
--(UIButton *)iconButton{
-    if (!_iconButton) {
-        _iconButton = [[UIButton alloc] init];
-        _iconButton.backgroundColor = [UIColor clearColor];
-        _iconButton.clipsToBounds = YES;
-        _iconButton.layer.cornerRadius = 8;
-        [_iconButton addTarget:self action:@selector(clickAction) forControlEvents:UIControlEventTouchUpInside];
-        _iconButton.adjustsImageWhenHighlighted = NO;
-    }
-    return _iconButton;
-}
+//-(UIButton *)iconButton{
+//    if (!_iconButton) {
+//        _iconButton = [[UIButton alloc] init];
+//        _iconButton.backgroundColor = [UIColor clearColor];
+//        _iconButton.clipsToBounds = YES;
+//        _iconButton.layer.cornerRadius = 8;
+//        [_iconButton addTarget:self action:@selector(clickAction) forControlEvents:UIControlEventTouchUpInside];
+//        _iconButton.adjustsImageWhenHighlighted = NO;
+//    }
+//    return _iconButton;
+//}
 
-- (UILabel *)nameLabel{
-    if (!_nameLabel) {
-        _nameLabel = [UILabel new];
-        _nameLabel.textColor = KHexColor(@"393939");
-        _nameLabel.font = KRegularFont(16);
-    }
-    return _nameLabel;
-}
+//- (UILabel *)nameLabel{
+//    if (!_nameLabel) {
+//        _nameLabel = [UILabel new];
+//        _nameLabel.textColor = KHexColor(@"393939");
+//        _nameLabel.font = KRegularFont(16);
+//    }
+//    return _nameLabel;
+//}
+//
+//- (TSRecomendGoodsView *)goodsView{
+//    if (!_goodsView) {
+//        _goodsView = [TSRecomendGoodsView new];
+//    }
+//    return _goodsView;
+//}
 
-- (TSRecomendGoodsView *)goodsView{
-    if (!_goodsView) {
-        _goodsView = [TSRecomendGoodsView new];
+- (NSMutableArray *)iconButtons{
+    if (!_iconButtons) {
+        _iconButtons = @[].mutableCopy;
     }
-    return _goodsView;
+    return _iconButtons;
 }
 @end
