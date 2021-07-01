a852aa40853615f2d7c0c3fc90b40b948bc6292a
余海 <hai6.yu@tcl.com>
2021-07-01 19:15:09 +0800

    优化 键盘 弹出 类型

--- TShopMall/TShopMall/Features/Setting/View/TSChangeMobileCell.m
+++ TShopMall/TShopMall/Features/Setting/View/TSChangeMobileCell.m
@@ -228,7 +228,7 @@
     if (_codeTextField == nil) {
         UITextField *codeTextField = [[UITextField alloc] init];
         _codeTextField = codeTextField;
-        _codeTextField.keyboardType = UIKeyboardTypeDefault;
+        _codeTextField.keyboardType = UIKeyboardTypeNumberPad;
         _codeTextField.textColor = KHexColor(@"#2D3132");
         _codeTextField.font = KRegularFont(16);
         _codeTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"验证码" attributes:@{NSForegroundColorAttributeName : KHexAlphaColor(@"#2D3132", 0.2)}];
