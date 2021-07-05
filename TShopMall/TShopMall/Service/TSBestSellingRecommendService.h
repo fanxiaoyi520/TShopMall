71f9379ed0ff4dbe81579a244d28ffcc94c5b9dd
余海 <hai6.yu@tcl.com>
2021-07-05 16:54:46 +0800

    调整文件目录

---
+++ TShopMall/TShopMall/Service/TSBestSellingRecommendService.h
@@ -0,0 +1,18 @@
+//
+//  TSBestSellingRecommendService.h
+//  TShopMall
+//
+//  Created by sway on 2021/6/26.
+//
+
+#import <Foundation/Foundation.h>
+#import "TSServicesManager.h"
+#import "TSRecomendModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TSBestSellingRecommendService : NSObject<TSBestSellingRecommendServiceProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
