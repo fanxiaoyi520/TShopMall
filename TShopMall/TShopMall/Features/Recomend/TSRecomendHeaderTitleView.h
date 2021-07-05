d079f1be242650a959f3fc82691a4b8978abe4a3
张厚琪 <houqi.zhang@tcl.com>
2021-07-05 16:18:07 +0800

    搜索页面修改问题

---
+++ TShopMall/TShopMall/Features/Recomend/TSRecomendHeaderTitleView.h
@@ -0,0 +1,17 @@
+//
+//  TSRecomendHeaderTitleView.h
+//  TShopMall
+//
+//  Created by 橙子 on 2021/7/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TSRecomendHeaderTitleView : UICollectionReusableView
+
+- (void)updateTitle:(NSString *)title textAlignment:(NSTextAlignment)textAlignment;
+@end
+
+NS_ASSUME_NONNULL_END
