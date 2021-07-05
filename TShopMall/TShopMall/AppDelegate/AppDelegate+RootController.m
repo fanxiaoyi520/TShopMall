c9a1c3c5940642f61de13cb3362f838c8995c178
陈结 <418280441@qq.com>
2021-07-05 09:58:52 +0800

    tshybridcontroller添加present模式

--- TShopMall/TShopMall/AppDelegate/AppDelegate+RootController.m
+++ TShopMall/TShopMall/AppDelegate/AppDelegate+RootController.m
@@ -87,6 +87,7 @@
     else{
         
         TSHybridViewController *web = [[TSHybridViewController alloc] initWithURLString:[agreementModel.serverUrl stringByAppendingString:@"&mode=webview"]];
+        web.isPresent = YES;
         TSBaseNavigationController *nav1 = [[TSBaseNavigationController alloc] initWithRootViewController:web];
         nav1.modalPresentationStyle = UIModalPresentationFullScreen;
         web.delegate = self;
